7344091701d126151ab941f3b7494ea6faac9852
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2023-12-20 14:16:42 -0330

    storing search data to postgres table search_data

--- services/pg-search.js
+++ services/pg-search.js
@@ -1,12 +1,31 @@
 const dal = require('./pg-database');
 
-const search = async (searchString) => {
+const search = async (searchString, username) => {
   if (DEBUG) console.log('search() function called');
+  console.log('user = ' + username);
+
+  // Ensure that the search_data table exists
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS search_data (
+        search_id SERIAL PRIMARY KEY,
+        username VARCHAR(100),
+        search_string VARCHAR(100),
+        search_time TIMESTAMP DEFAULT NOW()
+    );
+  `;
+  await dal.query(createTableQuery);
 
   const sql = `SELECT * FROM movies \
                WHERE title LIKE $1 OR genre LIKE $1 OR description LIKE $1`;
   try {
     const searchResults = await dal.query(sql, [`%${searchString}%`]);
+
+    // Insert the search string into the search_data table
+    const insertSearchQuery = `
+        INSERT INTO search_data (username, search_string)
+        VALUES ($1, $2);
+    `;
+    await dal.query(insertSearchQuery, [username, searchString]);
 
     // If the searchResults array is empty, log a message saying so
     if (searchResults.rows.length === 0) {
